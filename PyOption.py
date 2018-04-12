ad437b6e7ef690477bc71e6f109bc57224b2970b
michael kost <michaelrkost@outlook.com>
2018-04-12 11:45:59 -0400

    update

---
+++ PyOption.py
@@ -0,0 +1,18 @@
+from ib_insync import *
+import bullSpreadViewSmall as bsv
+import sys
+from PyQt5 import QtCore, QtWidgets
+# ========================================================
+
+
+def main():
+    app = QtWidgets.QApplication(sys.argv)
+    MainWindow = QtWidgets.QMainWindow()
+    ui = bsv.Ui_MainWindow()
+    ui.setupUi(MainWindow)
+    MainWindow.show()
+    sys.exit(app.exec_())
+
+
+if __name__ == "__main__":
+    main()
