932bee0e59891daa8b95155c4b6ff5d6046814d3
michael kost <michaelrkost@outlook.com>
2018-05-20 16:40:24 -0400

    updates

--- buildVerticalSpreadViews.py
+++ buildVerticalSpreadViews.py
@@ -56,11 +56,11 @@ def get_underlying_info(aTableWidget):
     # set the type of Price Data to receive
     #   - Frozen market data is the last data recorded at market close.
     #   - Last market data is the last data set, which may be empty after hours
-    aTableWidget.ib.reqMarketDataType(aTableWidget.marketDataType())
+    aTableWidget.ib.reqMarketDataType(marketDataType(aTableWidget))
 
     # from the GUI radio buttons determine if this a Stock/Index/Option and get the underlying
     # and create a Contract
-    aSecurityType=aTableWidget.security_type(the_underlying, the_exchange)
+    aSecurityType = security_type(aTableWidget, the_underlying, the_exchange)
 
     # then if securityType == Stock get Friday Expiry if Index get Thursday Expiry
     theExpiry = aTableWidget.comboBox_Expiry.currentText()
@@ -87,11 +87,11 @@ def get_underlying_info(aTableWidget):
         # create a new optionClass instance
         aTableWidget.an_option_spread = optionSpreadsClass.OptionSpreads(a_qualified_contract, aTableWidget.ib)
         # Fully qualify the option
-        aTableWidget.an_option_spread.qualify_option_chain(aTableWidget.right(), theExpiry,
+        aTableWidget.an_option_spread.qualify_option_chain(right(aTableWidget), theExpiry,
                                                            theStrikePriceRange, theStrikePriceMultiple)
 
         # Display the contracts
-        aTableWidget.displayContracts(aTableWidget.an_option_spread.optionContracts)
+        displayContracts(aTableWidget, aTableWidget.an_option_spread.optionContracts)
 
         the_underlyingOutput = ' {} / Last Price: {:>7.2f}'.format(aTableWidget.an_option_spread.a_Contract.symbol,
                                                                    aTableWidget.an_option_spread.theUnderlyingReqTickerData.last)
@@ -102,8 +102,168 @@ def get_underlying_info(aTableWidget):
         aTableWidget.an_option_spread.buildGreeks()
 
         # logger.logger.info("Display Greeks")
-        aTableWidget.displayGreeks(aTableWidget.an_option_spread)
+        displayGreeks(aTableWidget, aTableWidget.an_option_spread)
         aTableWidget.an_option_spread.buildBullPandas()
-        aTableWidget.displayBullSpread(aTableWidget.an_option_spread)
+        displayBullSpread(aTableWidget, aTableWidget.an_option_spread)
         aTableWidget.an_option_spread.buildCallRatioSpread()
 
+def displayContracts(aTableWidget, contracts):
+    contractsLen = len(contracts)
+    aTableWidget.tableWidget.setRowCount(contractsLen)
+
+    # clear the tables for new data...
+    aTableWidget.tableWidget.clearContents()
+    aTableWidget.tableWidget_OptionGreeks.clearContents()
+    # Items are created outside the table (with no parent widget)
+    # and inserted into the table with setItem():
+    theRow = 0
+    for aContract in contracts:
+        # if Contract ID is 0 the Not Valid
+        if aContract.conId == 0:
+            aTableWidget.tableWidget.setItem(theRow, 0, QtWidgets.QTableWidgetItem('Not Valid Contract'))
+        else:
+            aTableWidget.tableWidget.setItem(theRow, 0, QtWidgets.QTableWidgetItem(str(aContract.conId)))
+        # set the remaining data
+            aTableWidget.tableWidget.setItem(theRow, 1, QtWidgets.QTableWidgetItem(aContract.symbol))
+            aTableWidget.tableWidget.setItem(theRow, 2, QtWidgets.QTableWidgetItem(
+            dateUtils.month3Format(aContract.lastTradeDateOrContractMonth)))
+            aTableWidget.tableWidget.setItem(theRow, 3, QtWidgets.QTableWidgetItem('{:>7.0f}'.format(aContract.strike)))
+            aTableWidget.tableWidget.setItem(theRow, 4, QtWidgets.QTableWidgetItem(aContract.right))
+        #
+        theRow = theRow + 1
+
+def displayGreeks(aTableWidget, contracts):
+
+    greeksLen = len(contracts.right) * ( len(contracts.theStrikes * len(contracts.theExpiration)))
+    aTableWidget.tableWidget_OptionGreeks.setRowCount(greeksLen)
+    aTableWidget.tableWidget_OptionGreeks.clearContents()
+    # Items are created outside the table (with no parent widget)
+    # and inserted into the table with setItem():
+    theRow = 0
+    anExpriy = contracts.theExpiration
+    for aRight in contracts.right:
+        for aStrike in contracts.theStrikes:
+            aTableWidget.tableWidget_OptionGreeks.setItem(theRow, 0, QtWidgets.QTableWidgetItem(
+                '{:d}'.format(int(contracts.optionPrices.loc[(aRight, anExpriy, aStrike), 'ID']))))
+            aTableWidget.tableWidget_OptionGreeks.setItem(theRow, 1, QtWidgets.QTableWidgetItem(aRight))
+            aTableWidget.tableWidget_OptionGreeks.setItem(theRow, 2,
+                                                  QtWidgets.QTableWidgetItem(dateUtils.month3Format(anExpriy)))
+            aTableWidget.tableWidget_OptionGreeks.setItem(theRow, 3, QtWidgets.QTableWidgetItem(str(aStrike)))
+            aTableWidget.tableWidget_OptionGreeks.setItem(theRow, 4, QtWidgets.QTableWidgetItem(
+                '{:>7.2f}'.format(contracts.optionPrices.loc[(aRight, anExpriy, aStrike),'Price'])))
+            aTableWidget.tableWidget_OptionGreeks.setItem(theRow, 5, QtWidgets.QTableWidgetItem(
+                '{:>2.2%}'.format(contracts.optionPrices.loc[(aRight, anExpriy, aStrike),'ImpliedVol'])))
+            aTableWidget.tableWidget_OptionGreeks.setItem(theRow, 6, QtWidgets.QTableWidgetItem(
+                '{:>.6f}'.format(contracts.optionPrices.loc[(aRight, anExpriy, aStrike), 'Gamma'])))
+            aTableWidget.tableWidget_OptionGreeks.setItem(theRow, 7, QtWidgets.QTableWidgetItem(
+                '{:>.6f}'.format(contracts.optionPrices.loc[(aRight, anExpriy, aStrike), 'Delta'])))
+            aTableWidget.tableWidget_OptionGreeks.setItem(theRow, 8, QtWidgets.QTableWidgetItem(
+                '{:>7.2f}'.format(contracts.optionPrices.loc[(aRight, anExpriy, aStrike),'TimeVal'])))
+
+            theRow += 1
+
+def displayBullSpread(aTableWidget, contracts):
+    # todo does this work for Puts??
+
+    aTableWidget.tableWidget_BullSpread.setRowCount(contracts.bullCallSpreads.shape[0])
+
+    aTableWidget.tableWidget_BullSpread.clearContents()
+
+    theRow = 0
+    for aStrikeL in contracts.theStrikes:
+        aTableWidget.tableWidget_BullSpread.setItem(theRow, 0, QtWidgets.QTableWidgetItem('{:>d}'.format(aStrikeL)))
+        aTableWidget.tableWidget_BullSpread.item(theRow, 0).setBackground(QtGui.QColor("lightBlue"))
+        for aStrikeH in contracts.theStrikes:
+            if aStrikeL < aStrikeH:
+                aTableWidget.tableWidget_BullSpread.setItem(theRow, 1, QtWidgets.QTableWidgetItem('{:>d}'.format(aStrikeH)))
+                aTableWidget.tableWidget_BullSpread.setItem(theRow, 2, QtWidgets.QTableWidgetItem(
+                                                    '{:>7.2f}'.format(contracts.bullCallSpreads.loc[(aStrikeL,
+                                                                                                     aStrikeH),
+                                                                                                    'Loss$'])))
+                aTableWidget.tableWidget_BullSpread.setItem(theRow, 3,QtWidgets.QTableWidgetItem(
+                                                    '{:>7.2f}'.format(contracts.bullCallSpreads.loc[(aStrikeL,
+                                                                                                     aStrikeH),
+                                                                                                    'Max$'])))
+                theRow += 1
+
+def right(aTableWidget):
+    if aTableWidget.radioButton_Call.isChecked():
+        return configIB.CALL_RIGHT
+    else:
+        return configIB.PUT_RIGHT
+
+def marketDataType(aTableWidget):
+    if aTableWidget.radioButton_MktDataType_Frozen.isChecked():
+        return configIB.MARKET_DATA_TYPE_FROZEN
+    else:
+        return configIB.MARKET_DATA_TYPE_LIVE
+
+def security_type(aTableWidget, the_underlying, the_exchange):
+    """ from the GUI radio buttons determine if this a Stock/Index/Option and get the underlying.
+    Create Contract.
+    :param the_underlying: Stock/Index
+    :param the_exchange: CBOE etc
+    :return:
+    """
+    if aTableWidget.radioButton_Index.isChecked():
+        a_underlying = Index(the_underlying, the_exchange, 'USD')
+        aTableWidget.securityType = "IND"
+    elif aTableWidget.radioButton_Stock.isChecked():
+        a_underlying = Stock(the_underlying, the_exchange, 'USD')
+        aTableWidget.securityType = "STK"
+    else:
+        print('<<<< in bullSpreadViewSmall.get_underlying_info(self)>>>>> Option Radio not !completed!')
+    return a_underlying
+
+def updateConnect(aTableWidget):
+    aTableWidget.qualifyContracts.clicked.connect(lambda: get_underlying_info(aTableWidget))
+    aTableWidget.pushButton_updateNumberOfContracts.clicked.connect(lambda: updateBullContracts(aTableWidget))
+
+    aTableWidget.connectToIB.triggered.connect(lambda: onConnectButtonClicked(aTableWidget))
+    aTableWidget.actionIron_Condor.triggered.connect(lambda: updateToIronCondorWidget(aTableWidget))
+    aTableWidget.actionVertical_Spreads.triggered.connect(lambda: updateToVerticalSpreadWidget(aTableWidget))
+
+
+def updateBullContracts(aTableWidget):
+    try:
+        aTableWidget.an_option_spread
+    except NameError:
+        aTableWidget.statusbar.showMessage(".....need to get a contract first.....")
+    else:
+        theContractCount = aTableWidget.spinBox_numberOfContracts.value()
+        aTableWidget.an_option_spread.updateBullSpreads(theContractCount)
+        displayBullSpread(aTableWidget, aTableWidget.an_option_spread)
+        aTableWidget.an_option_spread.updateCallRatioSpread(theContractCount)
+
+
+def onConnectButtonClicked(self):
+    if self.connectToIB.isChecked():
+        self.ib.connect(configIB.IB_API_HOST,
+                    configIB.IB_PAPER_TRADE_PORT,
+                    configIB.IB_API_CLIENTID_1)
+        # TODO automate the Close/Frozen data from the GUI and the Client ID
+        # todo add clientID to menu
+        self.statusbar.showMessage("Connected to IB Paper and client #1")
+    else:
+        self.ib.disconnect()
+        self.statusbar.showMessage("Disconnected from IB")
+
+def updateToIronCondorWidget(aTableWidget):
+    if aTableWidget.actionIron_Condor.isChecked():
+        aTableWidget.stackedWidget.setCurrentIndex(1)
+        aTableWidget.actionVertical_Spreads.setChecked(False)
+        aTableWidget.actionIron_Condor.setChecked(True)
+    else:
+        aTableWidget.stackedWidget.setCurrentIndex(0)
+        aTableWidget.actionVertical_Spreads.setChecked(True)
+        aTableWidget.actionIron_Condor.setChecked(False)
+
+def updateToVerticalSpreadWidget(aTableWidget):
+    if aTableWidget.actionVertical_Spreads.isChecked():
+        aTableWidget.stackedWidget.setCurrentIndex(0)
+        aTableWidget.actionVertical_Spreads.setChecked(True)
+        aTableWidget.actionIron_Condor.setChecked(False)
+    else:
+        aTableWidget.stackedWidget.setCurrentIndex(1)
+        aTableWidget.actionVertical_Spreads.setChecked(False)
+        aTableWidget.actionIron_Condor.setChecked(True)
